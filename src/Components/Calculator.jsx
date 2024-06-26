de4f2fb7a0018faccc6a26c39069a0764f4731d8
StichVickers <152029648+StichVickers@users.noreply.github.com>
2024-06-26 21:07:17 +0300

    Update Calculator.jsx

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -1,6 +1,6 @@
 import './Calculator.css';
 import React, { useState, useEffect } from "react";
-import { useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router';
 function Calc() {
     const [area, setArea] = useState(0);
     const [systemType, setSystemType] = useState('');
