3f984494eb3088e95d4dc5b871e087af7819aa99
StichVickers <152029648+StichVickers@users.noreply.github.com>
2024-06-26 21:08:53 +0300

    Update Lk.jsx

--- src/Lk.jsx
+++ src/Lk.jsx
@@ -1,18 +1,11 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Lk from './Components/Lk';
-import Calculator from '../src/Components/Calculator';
 function calc() {
   
   const [count, setCount] = useState(0)
   return (
     <>
-      <Router>
-      <Switch>
-        <Route path="/Calc" exact component={Calculator} />
-        <Route path="/Profile" component={Profile} />
-      </Switch>
-    </Router>
       <Lk></Lk>
     </>
   )
